61d94334051eebf255f2f2476fb303af1366d936
coffee-dan <coffee-dan@protonmail.com>
2020-12-28 14:57:29 -0600

    Loading screen and custom fonts

--- App.js
+++ App.js
@@ -1,13 +1,40 @@
 import React, { useState } from 'react'
 import { StyleSheet, View } from 'react-native'
+import * as Font from 'expo-font'
+import AppLoading from 'expo-app-loading'
+
 import Header from './components/header'
 import StartGameScreen from './screens/start-game'
 import GameScreen from './screens/game'
 import GameOverScreen from './screens/game-over'
 
-export default function App() {
+// if it is needed later try to install expo-font
+// expo install expo-font // or // npm install --save expo-font
+
+// loadAsync returns a Promise which must be resolved before rendering anything
+// that depends on these assets
+const fetchFonts = () => {
+	return Font.loadAsync({
+		'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+		'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf'),
+	})
+}
+
+// APP COMPONENT
+function App() {
 	const [userNumber, setUserNumber] = useState()
 	const [guessRounds, setGuessRounds] = useState(0)
+	const [dataLoaded, setDataLoaded] = useState(false)
+
+	if (!dataLoaded) {
+		return (
+			<AppLoading
+				startAsync={fetchFonts}
+				onFinish={() => setDataLoaded(true)}
+				onError={err => console.log(err)}
+			/>
+		)
+	}
 
 	const configureNewGameHandler = () => {
 		// for restarting
@@ -52,3 +79,5 @@ const styles = StyleSheet.create({
 		flex: 1,
 	},
 })
+
+export default App
