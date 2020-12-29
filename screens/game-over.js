7f2836b0b8f31ef5e89f731f08668a5af61e853f
coffee-dan <coffee-dan@protonmail.com>
2020-12-28 18:25:25 -0600

    End screen image
    Some explanation of the thought process and certain functionalities

--- screens/game-over.js
+++ screens/game-over.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, StyleSheet, Button } from 'react-native';
+import { View, StyleSheet, Button, Image } from 'react-native';
 
 import BodyText from '../components/bodyText';
 import TitleText from '../components/titleText';
@@ -9,6 +9,19 @@ function GameOverScreen({ roundsNumber, userNumber, onRestart }) {
 	return (
 		<View style={styles.screen}>
 			<TitleText>The Game is Over</TitleText>
+			<View style={styles.imageContainer}>
+				{/* bringing in images provides information about image size that
+				can be used for styling */}
+				<Image
+					// all images have build in fade-in animation that can be modified with
+					// fadeDuration=''
+					// web images don't come with image size information so that must be added manually
+					// source={{uri: 'http://wccftech.com/amd-zen-summit-ridge-launch-q4-2016/'}}
+					source={require('../assets/success.png')}
+					style={styles.image}
+					resizeMode="cover"
+				/>
+			</View>
 			<BodyText>Number of rounds: {roundsNumber}</BodyText>
 			<BodyText>Number was: {userNumber}</BodyText>
 			<Button title="NEW GAME" onPress={onRestart} />
@@ -22,6 +35,19 @@ const styles = StyleSheet.create({
 		flex: 1,
 		justifyContent: 'center',
 		alignItems: 'center',
+	},
+	imageContainer: {
+		width: 300,
+		height: 300,
+		borderRadius: 150,
+		borderWidth: 3,
+		borderColor: 'black',
+		overflow: 'hidden',
+		marginVertical: 30,
+	},
+	image: {
+		width: '100%',
+		height: '100%',
 	},
 });
 
